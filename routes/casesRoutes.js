bead7ee7315efa752b4c91a290fc5654b6c47028
mastersdental8 <omar.kmh.96@gmail.com>
2025-04-17 05:59:02 +0400

    add aproved

---
+++ routes/casesRoutes.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const { registerValidator } = require("../helper/validator");
+const {
+  getAllCases,
+  createCase,
+  getCaseById,
+  deleteCase,
+  updateCase,
+  updateProcessCase,
+  updateIsHoldCase,
+  getCasesByMonth,
+  getCaseSearch,
+  updateIsUrgentCase,
+  getCounter,
+  updateIsAprroveCase,
+} = require("../controllers/CaseController");
+
+// Get All Cases
+router.get("/", getAllCases);
+router.get("/counter", getCounter);
+router.get("/cases-by-month", getCasesByMonth);
+router.get("/search", getCaseSearch);
+// Get Single Case
+router.get("/:id", getCaseById);
+
+// Create a new Case
+router.post("/", createCase);
+// Delete Case
+router.delete("/:id", deleteCase);
+router.patch("/:id", updateCase);
+router.put("/:id/:section", updateProcessCase);
+router.put("/:id/hold/:isHold", updateIsHoldCase);
+router.put("/:id/approve/:isApprove", updateIsAprroveCase);
+router.put("/:id/urgent/:isUrgent", updateIsUrgentCase);
+module.exports = router;
