ab4ab77d470cf32e179910c9d21609a6817c93a2
Omarokmm <omar.kmh.96@gmail.com>
2026-08-02 04:07:42 +0400

    updat month getuser cases

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,11 +37,11 @@ const currentDate = new Date();
 year = currentDate.getFullYear();
 month = currentDate.getMonth() + 1;
   // Create start date for the 1st day of the month at 00:00:00
-  const startOfMonth = new Date(year, month - 3, 1); // month is 0-indexed, so subtract 1
+  const startOfMonth = new Date(year, month - 4, 1); // month is 0-indexed, so subtract 1
   startOfMonth.setHours(0, 0, 0, 0); // Start of the day
 
   // Create end date for the last day of the month at 23:59:59.999
-  const endOfMonth = new Date(year, month + 3, 0); // Get last day of the month
+  const endOfMonth = new Date(year, month + 4, 0); // Get last day of the month
   endOfMonth.setHours(23, 59, 59, 999); // End of the day
       // Retrieve cases created within the specified month range
       const cases = await Case.find({
